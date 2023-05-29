2ecea360330f2bcf098b42fed2bd62bd730c488d
Sandoval Güicho <sandovalguichoo@gmail.com>
2023-05-29 13:15:43 -0600

    configuracion-dependencias-ok

--- src/main/java/com/spring/cloud/gateway/SpringCloudGatewayApplication.java
+++ src/main/java/com/spring/cloud/gateway/SpringCloudGatewayApplication.java
@@ -23,4 +23,42 @@ public class SpringCloudGatewayApplication  {
  * - Configura las rutas de acceso a los microservicios
  */
 
-/* Configuracion con Oauth2*/
+
+
+/* 
+ * ========= Configuracion con Oauth2 =========
+ 1) Añadir las siguientes dependencias
+ 
+ 	Spring Security
+ 	CloudBoostrap
+ 	ConfigClient (Servidor de configuraciones)
+ 	
+ 	1.1) Modificamos la siguiente dependencia
+ 	
+ 	antes:
+ 		<dependency>
+			<groupId>org.springframework.cloud</groupId>
+			<artifactId>spring-cloud-starter</artifactId>
+		</dependency>
+		
+	despues:
+		<dependency>
+			<groupId>org.springframework.cloud</groupId>
+			<artifactId>spring-cloud-starter-bootstrap</artifactId>
+		</dependency>
+ 
+ 	1.2 Elimina la siguiente dependecia porque no es compatible
+ 		 spring-boot-starter-web
+ 		 
+ 		 
+ */
+
+
+/* Errores:
+	1) Please set spring.main.web-application-type=reactive or remove spring-boot-starter-web dependency.
+	Solo eliminala
+	
+
+ 
+ * 
+ */
